9fff0a128b85ff90c0dd1787357948b59b4c4b86
Suryawanshi <vissurya@bmc.com>
2022-01-11 16:24:29 +0530

    Changes have been committed.

---
+++ src/test/java/com/piedpiper/artifactbookzy/ArtifactBookzyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.piedpiper.artifactbookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactBookzyApplicationTests {
+  	@Test
+	void contextLoads() {
+	}
+/*	@Test
+	public void incompleteTest() {
+	    fail("Not yet implemented");
+	}*/
+}
